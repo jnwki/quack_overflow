3599c9b24b16939077786acbcbc628b44a601050
jnwcki <jude.nowicki@gmail.com>
2016-03-23 13:05:07 -0400

    created models

--- duckapp/models.py
+++ duckapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User')
+    score = models.IntegerField()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Question(models.Model):
+    asker = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=255)
+    question_text = models.TextField()
+    tags = models.ManyToManyField(Tag)
+
+
+class Answer(models.Model):
+    answerer = models.ForeignKey('auth.User')
+    question = models.ForeignKey(Question)
+    answer_text = models.TextField()
+    score = models.IntegerField()
