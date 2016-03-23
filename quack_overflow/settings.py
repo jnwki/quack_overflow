4297f2516e6c22ffc2e61e6a58223534add5a5fd
jnwcki <jude.nowicki@gmail.com>
2016-03-23 12:50:08 -0400

    working home page hello world

--- quack_overflow/settings.py
+++ quack_overflow/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'duckapp',
+    'bootstrap3',
 ]
 
 MIDDLEWARE_CLASSES = [
--- quack_overflow/urls.py
+++ quack_overflow/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from duckapp.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', IndexView.as_view(), name='index'),
 ]
